8db044e341971c8c8b789ef043d58c6024fe43d5
shubham <shubwar@gmail.com>
2024-06-28 20:48:55 +0530

    added login function

---
+++ login.py
@@ -0,0 +1,10 @@
+USERNAME = "shubham"
+PASSWORD = "shubham1995"
+
+def login(user, password):
+    if user == USERNAME and password == PASSWORD:
+        print("user logged in successfully")
+    else:
+        print("invalid credentials")
+
+    
